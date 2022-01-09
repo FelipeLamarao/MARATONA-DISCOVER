dca043dbbc242d8208459f13ca889dfb4d929233
Felipe Lamarao <31902070+FelipeLamarao@users.noreply.github.com>
2022-01-09 11:00:17 -0400

    main

--- scripts.js
+++ scripts.js
@@ -108,7 +108,7 @@ const DOM = {
 
 
     // OBJETO QUE AFETA DIRETAMEMENTE O HTML ==============================
-    innerHTMLTransaction(transaction) {
+    innerHTMLTransaction(transaction,index) {
         const CSSclass =  transaction.amount > 0 ? "income" : "expense"
 
         const amount = Utils.formatCurrency(transaction.amount)  //altera o valor da transaction - ele recebe o Utils.formatCurrency 
@@ -148,22 +148,125 @@ const DOM = {
 
 //converte os valores 
 const Utils = {
-    formatCurrency(value){
-        const signal = Number(value) < 0 ? "-" : "" // verifica se o valor for maior o signal e + se nao for sera negativo - 
+    formatAmount(value){
+        value = Number(value.replace(/\,\./g, "")) * 100
+        
+        return value
+    },
+
+    formatDate(date) {
+        const splittedDate = date.split("-")
+        return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`
+    },
+
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? "-" : ""
 
         value = String(value).replace(/\D/g, "")
-     
-        value = Number(value ) / 100
 
-        value = value.toLocaleString("pt-BR",{
-            style : "currency",
+        value = Number(value) / 100
+
+        value = value.toLocaleString("pt-BR", {
+            style: "currency",
             currency: "BRL"
         })
 
-        return signal + value
+       return signal + value
     }
 }
 
+
+
+
+const Form = {
+
+    //ESTRUTURA PARA VERIFICAR SE OS DADOS FORAM PREECHIDOS =========================================
+
+    // PEGA OS ELEMENTOS HTML    
+    description: document.querySelector('input#description'),
+    amount: document.querySelector('input#amount'),
+    date: document.querySelector('input#date'),
+
+    // VAI PEGAR OS VALORES INPUTADOS
+    getValues(){
+        return{
+            description: Form.description.value,
+            amount: Form.amount.value,
+            date: Form.date.value
+        }
+    },        
+
+     // verificar se todos os dados foram preenchidos 
+    validateFields(){
+        const {description , amount , date} = Form.getValues()
+
+        if( description.trim() === "" || description.amount === "" || description.date === "" ){
+            throw new Error ("Por favor , preencha todos os campos")
+        }
+    },
+
+   
+
+    // ESTRUTURA QUE FORMATA OS DADOS DO SUBMIT ===========================================================
+
+    formatValues(value){
+        let {description , amount , date} = Form.getValues()
+
+        amount = Utils.formatAmount(amount)
+
+        date = Utils.formatDate(date)
+
+
+        return{
+            description,
+            amount, 
+            date
+        }      
+
+    },
+
+    // ESTRUTURA QUE SALVA OS DADOS DO SUBMIT ===========================================================
+    
+    saveTransaction(transaction){
+        Transaction.add(transaction)
+    },
+
+    // LIMPA OS DADOS 
+
+    clearFields(){ 
+        Form.description.value = ""
+        Form.amount.value = ""
+        Form.date.value = ""
+    },
+
+    // ==================================================================================================
+
+
+
+    // OBJETO CHAMADA NO MOMENTO QUE É FEITO O INPUT - ONSUBMIT - 
+    submit(event){
+        event.preventDefault()
+
+        // verificação de erros o try faz a chamada caso tenha um erro o cath e chamaado e faz o alerta da mensagem
+        try{            
+            Form.validateFields()//VALIDA OS DADOS             
+            const transaction = Form.formatValues()//FORMATA OS DADOS,  
+            Form.saveTransaction(transaction) // adicionar os dados da transacao 
+            Form.clearFields() // limpa o formulariol
+            Modal.close() // fecha o modal 
+             
+        }catch(error){
+            alert(error.message)
+        }
+
+
+        
+        
+    }
+   
+
+
+}
 
 
 const App = {
@@ -182,17 +285,13 @@ const App = {
     },
 }
 
-const Form = {
-    submit(event){
-        event.preventDefault()
-    }
-}
-
 
 
 // CHAMADAS DE OBJETOS ======================================
 
 App.init()
 
-Transaction.remove(3)
+Transaction.remove(0)
+
+
 
